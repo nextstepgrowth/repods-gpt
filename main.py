aafc5c3a085c93a48123f6f46951bb8458f9f46e
jhmin95 <tesla52@naver.com>
2025-04-23 10:53:52 +0900

    fix: set_page_config 위치 변경

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from crud import download_prompts
 
 load_dotenv()
 
-download_prompts()
-
-prompts = get_prompts()
-
-st.title("Repods AI")
-
 st.set_page_config(
     page_title='Repods AI',
     layout='wide',
     initial_sidebar_state='expanded',
     page_icon='Repods_logo_mark-black.jpg',
 )
+
+download_prompts()
+
+prompts = get_prompts()
+
+st.title("Repods AI")
+
 
 
 st.switch_page('pages/1_page1.py')
